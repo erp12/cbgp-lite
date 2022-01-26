0aefdec985fe4238f02ab76c7d32d99299d6bbbe
erp12 <eddie.pantridge@gmail.com>
2022-01-26 15:10:42 -0500

    Various enhances and fixes
    Adds do and print functions
    Fixes simplification and expected stdout of cases
    Adds multiple loss fns per problem and excludes macros from args
    Fixes problem definitions

--- src/erp12/cbgp_lite/gp.clj
+++ src/erp12/cbgp_lite/gp.clj
@@ -1,20 +1,72 @@
 (ns erp12.cbgp-lite.gp
   (:require [taoensso.timbre :as log]
             [taoensso.timbre.appenders.core :as log-app]
+            [clj-fuzzy.levenshtein :as lev]
             [erp12.ga-clj.generational :refer [evolve]]
             [erp12.cbgp-lite.gp.pluhsy :as pl]
             [erp12.cbgp-lite.gp.individual :as indiv]
             [erp12.cbgp-lite.lang.compile :as c]
-            [erp12.cbgp-lite.lang.lib :as lib]))
+            [erp12.cbgp-lite.lang.lib :as lib])
+  (:import (java.io StringWriter)))
 
 (log/merge-config!
   {:output-fn (partial log/default-output-fn {:stacktrace-fonts {}})
    :appenders {:println (assoc (log-app/println-appender) :min-level :info)
-               :spit    (assoc (log-app/spit-appender {:fname "./errors.log"}) :min-level :debug)}})
+               ;:spit    (assoc (log-app/spit-appender {:fname "./errors.log"}) :min-level :debug)
+               }})
+
+(defmacro with-out-and-stdout
+  [form]
+  `(with-open [s# (StringWriter.)]
+     (binding [*out* s#]
+       {:output  ~form
+        :std-out (str s#)})))
+
+(defn evaluate-code
+  [{:keys [code arg-symbols cases loss-fns penalty]}]
+  (let [;; Create a Clojure function with the compiled code.
+        func (c/synth-fn arg-symbols code)
+        ;; Call the function on each training case.
+        behavior (->> cases
+                      (map :inputs)
+                      (map #(try
+                              (with-out-and-stdout (apply func %))
+                              (catch Exception e
+                                {:output e :std-out nil}))))
+        ;; Compute the error on each case.
+        errors (do
+                 ;; Debug log any errors thrown during program evaluation to help in
+                 ;; debugging the library of functions and their type annotations.
+                 (when-let [ex (first (filter #(instance? Exception %) behavior))]
+                   (log/debug {:ex   (class ex)
+                               :msg  (.getMessage ex)
+                               :code code}))
+                 (->> cases
+                      (mapcat (fn [actual expected]
+                                (conj
+                                  ;; For each loss function, compute the loss or give penalty.
+                                  (mapv #(if (or (nil? (:output actual))
+                                                 (instance? Exception (:output actual)))
+                                           penalty
+                                           (% (:output actual) (:output expected)))
+                                        loss-fns)
+                                  ;; Append std-out error, when expected.
+                                  (when (contains? expected :std-out)
+                                    (if (nil? (:std-out actual))
+                                      penalty
+                                      (lev/distance (:std-out expected) (:std-out actual))))))
+                              behavior)
+                      ;; When there is no std-out error, filter out the nils.
+                      (filter some?)
+                      vec))]
+    {:func        func
+     :behavior    behavior
+     :errors      errors
+     :total-error (apply + errors)}))
 
 (defn make-individual-factory
-  [{:keys [input->type return-type vars loss-fn penalty]}]
-  (let [arg-symbols (vec (keys input->type))
+  [{:keys [input->type return-type vars loss-fns penalty]}]
+  (let [arg-symbols (vec (sort (keys input->type)))
         type-environment (->> lib/library
                               (filter (fn [[symb _]] (contains? vars symb)))
                               (merge input->type)
@@ -28,33 +80,14 @@
                                :inputs    arg-symbols
                                :ret-type  return-type
                                :type-env  type-environment
-                               :dealiases lib/dealiases})
-            ;; Create a Clojure function with the compiled code.
-            func (c/synth-fn arg-symbols code)
-            ;; Call the function on each training case.
-            behavior (->> cases
-                          (map :inputs)
-                          (map #(try
-                                  (apply func %)
-                                  (catch Exception e e))))
-            ;; Compute the error on each case.
-            errors (do
-                     (when-let [ex (first (filter #(instance? Exception %) behavior))]
-                       (log/debug {:ex   (class ex)
-                                   :msg  (.getMessage ex)
-                                   :code code}))
-                     (->> cases
-                          (map :output)
-                          (mapv #(if (or (nil? %1) (instance? Exception %1))
-                                   penalty
-                                   (loss-fn %1 %2))
-                                behavior)))]
-        {:push        push
-         :code        code
-         :func        func
-         :behavior    behavior
-         :errors      errors
-         :total-error (apply + errors)}))))
+                               :dealiases lib/dealiases})]
+        (merge (evaluate-code {:code code
+                               :arg-symbols arg-symbols
+                               :cases cases
+                               :loss-fns loss-fns
+                               :penalty penalty})
+               {:push push
+                :code code})))))
 
 (defn run
   [{:keys [max-generations simplification-steps cases downsample-rate] :as opts}]
@@ -66,19 +99,21 @@
                                                    {:cases (random-sample downsample-rate cases)})
                              :individual-factory individual-factory
                              :individual-cmp     (comparator #(< (:total-error %1) (:total-error %2)))
-                             :stop-fn            (fn [{:keys [step best]}]
+                             :stop-fn            (fn [{:keys [step best new-best?]}]
                                                    (log/info "REPORT"
                                                              {:step       step
                                                               :best-error (:total-error best)
                                                               :best-code  (:code best)})
                                                    (cond
                                                      (= step max-generations) :max-generation-reached
-
                                                      ;; If the "best" individual has solved the subset of cases
                                                      ;; Test if on the full training set.
                                                      (zero? (:total-error best))
-                                                     (if (zero? (:total-error (individual-factory (:genome best) {:cases cases})))
+                                                     (if (and new-best? (zero? (:total-error (individual-factory (:genome best) {:cases cases}))))
                                                        :solution-found
+                                                       ;; If an individual solves a batch but not all training cases,
+                                                       ;; no individual can become the new best and the run will fail.
+                                                       ;; @todo Fix this in ga-clj somehow?
                                                        (log/info "Best individual solved a batch but not all training cases."))))
                              :mapper             pmap}
                             opts))
