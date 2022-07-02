726fc98fba4b5003cb52b17103c04ce0fcf9d337
erp12 <eddie.pantridge@gmail.com>
2022-07-02 19:49:45 -0400

    Adds simulated annealing benchmarking

---
+++ benchmarks/erp12/cbgp_lite/benchmark/simulated_annealing.clj
@@ -0,0 +1,87 @@
+(ns erp12.cbgp-lite.benchmark.simulated-annealing
+  (:require [erp12.cbgp-lite.benchmark.utils :as bu]
+            [erp12.cbgp-lite.search.individual :as i]
+            [erp12.cbgp-lite.search.pluhsy :as pl]
+            [erp12.cbgp-lite.task :as task]
+            [erp12.ga-clj.search.simulated-annealing :as sa]
+            [erp12.ga-clj.toolbox :as tb]
+            [taoensso.timbre :as log]
+            [taoensso.timbre.appenders.core :as log-app]))
+
+(log/merge-config!
+  {:output-fn (partial log/default-output-fn {:stacktrace-fonts {}})
+   :appenders {:println (assoc (log-app/println-appender) :min-level :debug)
+               ;:spit    (assoc (log-app/spit-appender {:fname "./errors.log"}) :min-level :debug)
+               }})
+
+(def default-config
+  {:n-train              100
+   :n-test               300
+   ;; Comparable to GA: population size * max generations
+   :max-steps            (* 1000 300)
+   :umad-rate            0.1
+   :min-genome-size      50
+   :max-genome-size      250
+   :simplification-steps 2000
+   :report-period        1000})
+
+(defn run
+  [opts]
+  ;(doseq [[k v] opts]
+  ;  (println k ":" (class k) " -> " v ":" (class v)))
+  (let [config (merge default-config opts)
+        task (-> opts
+                 (assoc :config config)
+                 bu/read-problem
+                 task/enhance-task
+                 (assoc :evaluate-fn i/evaluate-full-behavior))
+        opts (merge config task)
+        individual-factory (i/make-individual-factory (-> opts
+                                                          (assoc :cases (:train task))
+                                                          (dissoc :train :test)))
+        {:keys [best result]} (sa/run {;; Function for generating random genomes.
+                                       :genome-factory     #(pl/random-plushy-genome opts)
+                                       ;; Function for converting genomes into compiled Clojure functions
+                                       ;; and associated metadata for tracking progress (number of evaluations).
+                                       :individual-factory individual-factory
+                                       ;;
+                                       :mutate             (let [umad (tb/make-size-neutral-umad
+                                                                        {:rate           (:umad-rate opts)
+                                                                         :genetic-source (:genetic-source opts)})]
+                                                             #(umad (:genome %)))
+                                       ;; We compare individuals on the basis of the total error.
+                                       :error-fn           :total-error
+                                       ;; Predicate function for determining when search should stop.
+                                       :stop-fn            (let [{:keys [report-period max-steps]} opts]
+                                                             (fn [{:keys [step individual temp]}]
+                                                               (when (zero? (mod step report-period))
+                                                                 (println "Step:" step "\tTemp:" temp "\tBest Code:" (:code individual)))
+                                                               (cond
+                                                                 (= (:error individual) 0) :solution-found
+                                                                 (>= step max-steps) :max-step-reached)))
+                                       :max-steps          (:max-steps opts)})
+        ;; Simplify the best individual seen during evolution.
+        best (i/simplify {:individual           best
+                          :simplification-steps (:simplification-steps config)
+                          :individual-factory   individual-factory})
+        ;; Evaluate the final program on the unseen test cases.
+        {:keys [solution?]} (i/evaluate-until-first-failure {:code        (:code best)
+                                                             :arg-symbols (:arg-symbols task)
+                                                             :cases       (:test task)
+                                                             :loss-fns    (:loss-fns task)})]
+    (if (= :solution-found result)
+      (do
+        (log/info "SOLUTION FOUND")
+        (if solution?
+          (log/info "SOLUTION GENERALIZED")
+          (log/info "SOLUTION FAILED TO GENERALIZE")))
+      (log/info "SOLUTION NOT FOUND"))
+    (:func best)))
+
+(comment
+
+  (run {:suite-ns 'erp12.cbgp-lite.benchmark.suite.psb
+        :data-dir "data/psb/"
+        :problem  "vectors-summed"})
+
+  )
