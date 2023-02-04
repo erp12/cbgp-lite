f643bd0fd6a64c9f5a4a309f0f976758383eddb2
Tom Helmuth <thelmuth@hamilton.edu>
2023-02-03 21:49:30 -0500

    Added polymorphic problem

--- benchmarks/erp12/cbgp_lite/benchmark/suite/composite.clj
+++ benchmarks/erp12/cbgp_lite/benchmark/suite/composite.clj
@@ -16,11 +16,11 @@
   (+ (get input1 input2)
      (get input1 input3)))
 
-(defn sum-2-vals-case-generator
-  "Produce a map like this {:inputs [4 400000], :output 4}"
-  []
-  (let [key-gen (bu/string-generator 10)
-        key1 (key-gen)
+(defn sum-2-vals-case-generator-generic
+  "Produce a map of inputs and outputs.
+   Works with any key generator functyion key-gen"
+  [key-gen]
+  (let [key1 (key-gen)
         key2 (key-gen)
         num-kvs (rand-int 49) ; along with two guaranteed keys, this makes at most 50 kv pairs
         the-keys (repeatedly num-kvs key-gen)
@@ -31,6 +31,22 @@
         inputs [input-map key1 key2]]
     {:inputs inputs
      :output (apply sum-2-vals-solution-fn inputs)}))
+
+(defn sum-2-vals-case-generator
+  "Produce a map of inputs and outputs"
+  []
+  (sum-2-vals-case-generator-generic (bu/string-generator 10)))
+
+(defn sum-2-vals-polymorphic-case-generator
+  []
+  (let [all-generators [(bu/string-generator 10)
+                        (bu/int-generator 1000)
+                        #(bu/rand-char)
+                        rand
+                        #(vec (repeatedly (inc (rand-int 16)) bu/rand-bool)) ;; vector of booleans
+                        #(list (bu/rand-char) (rand-int-range -10 10)) ;; tuple containing a char and an integer
+                        ]]
+    (sum-2-vals-case-generator-generic (rand-nth all-generators))))
 
 (defn problems
   [{:keys [penalty]}]
@@ -58,7 +74,19 @@
                       :type {:type 'string?}}]
        :case-generator sum-2-vals-case-generator
        :loss-fns    [bu/absolute-distance]}
-      
+
+      "sum-2-vals-polymorphic"
+      {:description "Given a map from 'a to ints and two 'a that are
+                     keys of the map, look up the values associated with those keys
+                     in the map and return their sum."
+       :input->type {'input1 {:type :map-of, :key (lib/s-var 'a), :value {:type 'int?}}
+                     'input2 (lib/s-var 'a)
+                     'input3 (lib/s-var 'a)}
+       :ret-type    {:type 'int?}
+       :other-types [{:type 'boolean?} {:type 'string?} {:type 'char?} {:type 'double?}]
+       :extra-genes [{:gene :lit, :val 0, :type {:type 'int?}}]
+       :case-generator sum-2-vals-polymorphic-case-generator
+       :loss-fns    [bu/absolute-distance]}
       }
 
      ;; This adds nil penalties to all loss functions
@@ -89,11 +117,12 @@
   (repeatedly 5 (partial rand-int-range 5 15))
   
   (sum-2-vals-case-generator)
- 
+
   
   (read-cases {:problem "sum-2-vals"
                :n-train 4
                :n-test 2})
   
-  
+  (sum-2-vals-polymorphic-case-generator)
+
   )
