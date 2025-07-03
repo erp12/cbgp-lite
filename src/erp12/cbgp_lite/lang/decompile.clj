d92e4ef4306e6fc530b5dff0a7c1494e57bca355
Sydney Chen <schen2@hamilton.edu>
2025-07-03 16:58:52 -0400

    more prep for ad-hoc decompilation

--- src/erp12/cbgp_lite/lang/decompile.clj
+++ src/erp12/cbgp_lite/lang/decompile.clj
@@ -16,9 +16,9 @@
    (let [_ (when verbose (println "PLUSHY:" genome))
          push (pl/plushy->push genome)
          _ (when verbose (println "PUSH:" push))
-         ast (::co/ast (co/push->ast {:push push
+         ast (::co/ast (:ast (co/push->ast {:push push
                                       :ret-type ret-type
-                                      :type-env lib/type-env}))
+                                      :type-env lib/type-env})))
          _ (when verbose (println "AST:" ast))
          form (ast/ast->form ast)
          _ (when verbose (println "FORM:" form))
@@ -159,11 +159,7 @@
    'zero-double?])
 
 (def work-without-change
-  [;; FP
-   'comp
-   'partial
-
-   ;; Common/Misc
+  [;; Common/Misc
    'not
    'not=
    'if
@@ -176,8 +172,6 @@
    'dec
    'abs
 
-   ;; Text
-
    ;; Collections
    'count
    'vec
@@ -186,10 +180,8 @@
    'last
    'empty?
    'contains?
-   'assoc
    'merge
    'disj
-   'get
    'update])
 
 (def ast-aliasing
@@ -204,11 +196,9 @@
 
    ;; Numeric
    'add '+
-   'sub '-
    'multiply '*
    'quotient 'quot
    'divide '/
-   'neg `lib/neg ; --> arity; minus w/ one arg
    'pow `lib/pow
    ; square (skip)
    'intCast 'int
@@ -227,28 +217,19 @@
    'isZero 'zero-int?
 
    ;; Text
-   ; `lib/join, 'str-join-sep --> arity; str-join-sep w/ 2 args
-   ; 'str, 'append-str --> arity; append-str w/ 2 args
    'charCast `lib/int->char
    'isWhitespace `lib/whitespace?
    'isDigit `lib/digit?
    'isLetter `lib/letter?
-   ; split-str-on-ws, split-str --> arity; split-str w/ 2 args
-   ; `lib/set-char (doesn't exist in clojure?)
-   'capitalize  `str/capitalize
-   'upper-case  `str/upper-case
-   'lower-case  `str/lower-case
+   ; `lib/set-char (doesn't exist in clojure)
+   'str/capitalize  `str/capitalize
+   'str/upper-case  `str/upper-case
+   'str/lower-case  `str/lower-case
    'toUpperCase  `lib/char-upper
    'toLowerCase  `lib/char-lower
-
-   ;; Boolean
-   ; 'and__5600__auto__  `lib/and
-   ; 'or__5602__auto__  `lib/or
    
    ;; Collections
-   ; 'mapv
-   ; `lib/map2v --> arity; mapv w/ 2 args
-   ; `lib/->map --> (?) arity
+   ; `lib/->map --> into {}
    'concat `lib/concat'
    'conj `lib/conj'
    'rest `lib/rest'
@@ -258,12 +239,10 @@
    'index-of `lib/index-of
    'filter `lib/filter'
    'remove `lib/remove'
-   ; `lib/remove-element --> arity; remove w/ 2 args
-   ; 'reduce, 'fold --> arity
+   ; `lib/remove-element --> type-specific
    'mapcat `lib/mapcat'
 
    ;; Text/Vec
-   'nth `lib/safe-nth
    'replace `lib/replace'
    'replace-first `lib/replace-first'
    'take `lib/take'
@@ -273,11 +252,8 @@
    'subvec `lib/safe-sub-coll'
 
    ;; Vector
-   ; ->vector (arity)
-   ; 'nth-or-else (arity; 'nth)
    ; `lib/occurrences-of (doesn't exist in clojure?)
    ; 'safe-assoc-nth (typed; assoc on a vec)
-   ; 'range (arity)
    'map-indexed `lib/mapv-indexed
    'distinct `lib/distinctv
    'sort-by `lib/sortv-by
@@ -285,110 +261,75 @@
    'zipmap 'zipmap
 
    ;; Set
-   ; ->set (arity)
-   'union `set/union
-   'difference `set/difference
-   'intersection `set/intersection
-   'subset? `set/subset?
-   'superset? `set/superset?
+   'set/union `set/union
+   'set/difference `set/difference
+   'set/intersection `set/intersection
+   'set/subset? `set/subset?
+   'set/superset? `set/superset?
    ; `lib/map-set (may delete; keep mapv-set)
    
    ;; Map
-   ; ->map (arity)
-   ; 'get-or-else (arity; 'get w/ 3 args)
    'keys `lib/keys-vec
    ; `lib/keys-set (doesn't exist in clojure?)
    'vals `lib/vals-vec})
 
-(def ast-number-aliasing
-  {
-  ;;  'add "add"
-  ;;  'sub "sub"
-  ;;  'multiply "mult"
-  ;;  'divide "div"
-  ;;  'quotient "quot"
-  ;;  'mod "mod"
-  ;;  'inc "inc"
-  ;;  'dec "dec"
-  ;;  'neg "neg" ; minus w/ one arg
-  ;;  'abs "abs"
-  ;;  'intCast 'int
-   })
-   ; 'intCast 'char->int
-
-(def ast-str-vec-aliasing
-  {;'first "first"
-   ;'last "last" 
-   'nth 'nth
-   ;'empty? "empty"
-   })
-
-  ;; Same Namespace issues as in ast-number-aliasing
-  ;; Make a new dictionary for namespace qualified (ns-q)
-  ;; symbols that are type dependent,  
-  ;; because ns-q vector symbols append v
-  ;; whereas other non-ns-q append nothing or vec 
-  ;; 'rest "`lib/rest"
-
-(def ast-collection-aliasing
-  {;'count "count"
-   })
-  ;;  'reduce "reduce"
-  ;;  'fold "fold"
-
 (def ast-arity-aliasing
-  ;; to include in this map:
-  ; - neg
-  ; `lib/join 'str-join-sep
-  ; 'str 'append-str
-  ; split-str-on-ws split-str
-  ; mapv map2v
-  ; ->map ->map1 ->map2 ->map3
-  ; ->vector1 ->vector2 ->vector3
-  ; ->set1 ->set2 ->set3 
-  ; range1 range2 range3
-  ; remove remove-element
-  ; reduce fold
-  ; nth nth-or-else
-  ; get get-or-else
-
-
-  {'str {1 'str
+  {'sub {1 'neg
+         2 '-
+         :default '-}
+   'str/join {1 `str/join
+              2 'str-join-sep
+              :default `str/join}
+   'str {1 'str
          2 `lib/concat-str
          :default `lib/concat-str}
-   'minus {1 'neg
-           2 'sub
-           :default 'sub}
-   ; Does not work on strings
-   'nth {2 `lib/safe-nth
-         3 'nth-or-else}
-
-   ; TO-DO:
-  ;;  'reduce {2 'reduce
-  ;;           3 'fold}
-
-   'vector {1 '->vector1
-            2 '->vector2
-            3 '->vector3
-            :default '->vector1}
-   ; TO-DO: add check for vec->set and map->set
-   'hash-set {1 '->set1
-              2 '->set2
-              3 '->set3
-              :default '->set1}
-   ; TO-DO: add check for vec->map and set->map
+   'str/split {1 `lib/split-str-on-ws
+               2 `lib/split-str
+               :default `lib/split-str}
+   'mapv {2 'mapv
+          3 `lib/map2v
+          :default 'mapv}
    'hash-map {2 '->map1
               4 '->map2
               6 '->map3
               :default '->map1}
+   'hash-set {1 '->set1
+              2 '->set2
+              3 '->set3
+              :default '->set1}
+   'vector {1 '->vector1
+            2 '->vector2
+            3 '->vector3
+            :default '->vector1}
    'range {1 'range1
            2 'range2
            3 'range3
-           :default 'range1}})
+           :default 'range1}
+   'reduce {2 'reduce
+            3 'fold
+            :default 'reduce}
+   'nth {2 `lib/safe-nth
+         3 'nth-or-else
+         :default `lib/safe-nth}
+   'get {2 'get
+         3 'get-or-else
+         :default 'get}})
 
-(def ast-namespace-qualified-type-aliasing
-  {'rest "rest"})
-  ;;  'concat "concat"
+(def type-specific-aliasing
+  [
+   'remove ; `lib/remove' `lib/remove-element
+   'assoc  ; 'assoc `lib/safe-assoc-nth
+  ])
+
+(def special-case-aliasing
+  [;; FP
+   'comp
+   'partial
+
+   ;; Boolean
+   ; `lib/and (macro)
+   ; `lib/or (macro)
+   ])
 
 (defn find-local
   "Takes a map or vec and recursively looks through it to find a map
@@ -416,113 +357,28 @@
 (defn get-fn-symbol
   "Finds the CBGP function name for this ast-fn-name"
   [ast-fn-name tag args task]
-  ;; (println "Firstp Task: " task ast-fn-name)
+  ;; (println "First Task: " task ast-fn-name)
   (cond
-    ;; Because of the phrasing, this needs to be hard coded
-    (= ast-fn-name 'intCast)
-    (if (= (str (:tag (first args))) "char")
-      'char->int
-      'int)
-
-    ;; functions with multiple arities to support
+    ;; arity-specific functions
     (contains? ast-arity-aliasing ast-fn-name)
     (let [arity-map (get ast-arity-aliasing ast-fn-name)
           fn-symbol (get arity-map
                          (count args)
                          (get arity-map :default))]
-      (if (contains? ast-number-aliasing fn-symbol)
-        (get-fn-symbol fn-symbol tag args task)
-        fn-symbol))
+      fn-symbol)
 
-    ;; numbers
-    (contains? ast-number-aliasing ast-fn-name)
-    (symbol (str (cond
-                   (= (str tag) "double") "double-"
-                   (= (str tag) "long") "int-"
-                   (= tag java.lang.Long) "int-"
-                   (= tag java.lang.Integer) "int-"
-                   (= ast-fn-name 'div) "int-"
-
-                   ;; Handle local making it so we don't know the type
-                   ;; Note; defaults to int? even when the type isn't
-                   ;; double? or int? This may occasionally break things
-                   :else
-                   (if (= 'double?
-                          (:type (get (:input->type task)
-                                      (:form (find-local args)))))
-                     "double-"
-                     "int-"))
-                 (get ast-number-aliasing ast-fn-name)))
-
-    ;; Vector stuff
-    (contains? ast-str-vec-aliasing ast-fn-name)
-    (symbol (str (get ast-str-vec-aliasing ast-fn-name)
-                 (cond
-                   (= (:tag (first args)) java.lang.String) "-str"
-                   (= (:tag (first args)) clojure.lang.APersistentVector) ""
-                   :else
-                   (if (= 'string?
-                          (:type (get (:input->type task)
-                                      (:form (find-local args)))))
-                     "-str"
-                     ""))
-
-                 (if (= 'empty? ast-fn-name)
-                   "?"
-                   "")))
-
-    ;;Vector-set-map
-    (contains? ast-collection-aliasing ast-fn-name)
-    (symbol (let [symb (str (get ast-collection-aliasing ast-fn-name)
-                            (cond
-                              (string? (:val (first args)))
-                              "-str"
-                              (set? (:val (first args)))
-                              "-set"
-                              (map? (:val (first args)))
-                              "-map"
-                              (vector? (:val (first args)))
-                              "-vec"
-                              :else
-                              (cond
-                                (= 'string?
-                                   (:type (get (:input->type task)
-                                               (:form (find-local args)))))
-                                "-str"
-                                (= :set
-                                   (:type (get (:input->type task)
-                                               (:form (find-local args)))))
-                                "-set"
-                                (= :map
-                                   (:type (get (:input->type task)
-                                               (:form (find-local args)))))
-                                "-map"
-                                (= :vector
-                                   (:type (get (:input->type task)
-                                               (:form (find-local args)))))
-                                "-vec"
-                                :else
-                                "-BAD")))]
-              ;; I didn't want to do this but CBGP naming 
-              ;; "conventions" forced my hand
-              (if (= symb "count-str")
-                "length"
-                symb)))
-
-    ;; rest only right now?
-    (contains? ast-namespace-qualified-type-aliasing ast-fn-name)
-    (symbol "erp12.cbgp-lite.lang.lib" (cond (get ast-namespace-qualified-type-aliasing ast-fn-name)
-                                             (cond
-                                               (= (str tag) "double")
-                                               (str "double-" (get ast-namespace-qualified-type-aliasing ast-fn-name))
-
-                                               (= (:tag (first args)) java.lang.String)
-                                               (str (get ast-namespace-qualified-type-aliasing ast-fn-name) "-str")
-
-                                               (= (:tag (first args)) clojure.lang.PersistentVector)
-                                               (str (get ast-namespace-qualified-type-aliasing ast-fn-name) "v")
-
-                                               :else (str "int-" (get ast-namespace-qualified-type-aliasing ast-fn-name)))))
+    ;; arg-type-specific functions
+    ; [!] does NOT work yet
+    (contains? type-specific-aliasing ast-fn-name)
+    (println "Type-specfic-aliasing not implemented yet!")
+    #_(cond
+      (if (= 'remove ast-fn-name) ; and first arg is a vector or string 
+        "`lib/remove-element"
+        "`lib/remove'")
+      (if (= 'assoc ast-fn-name) ; and first arg is a vector
+        "`lib/safe-assoc-nth"
+        "'assoc"
+        ))
 
     ;; main aliasing
     (contains? ast-aliasing ast-fn-name)
@@ -636,23 +492,22 @@
 ;;; Testing
 
 (comment
-
-  (compile-debugging (decompile-ast (ana.jvm/analyze '(+ 1 2))) {:type 'int?} true)
   
-  (compile-debugging (decompile-ast (ana.jvm/analyze '(nth [1.0 2.0 3.0] 10 4.04)))
+  (compile-debugging (decompile-ast (ana.jvm/analyze '(nth [1.0 2.0 3.0 5.0] 3 4.04)))
                      {:type 'double?})
-;;;; THESE DON'T WORK
-
-  (decompile-ast (ana.jvm/analyze '(nth [1 2 3] 2 5)))
+  
+;;;; THESE DON'T WORK 
 
   (compile-debugging (decompile-ast (ana.jvm/analyze '(or true false)))
                      {:type 'boolean?})
 
   (compile-debugging (decompile-ast (ana.jvm/analyze '(< 4 5 8)))
-                     {:type 'boolean?})
+                     {:type 'boolean?}) 
+  
+  (compile-debugging (decompile-ast (ana.jvm/analyze '((partial + 1) 2))) {:type 'int?} true)
 
   ;;; misc stuff
-
+  
   (ana.jvm/analyze '(fn [x] (+ x 1)))
 
   (map (fn [x] (+ x 1))
@@ -681,5 +536,6 @@
 
   (ana.jvm/analyze '(hash-map "a" 1))
 
-  (ana.jvm/analyze 'count))
+  (ana.jvm/analyze 'count)
+  )
 
